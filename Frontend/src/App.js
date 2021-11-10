7423a3078e81790776f534a85c7f49e0fbd72d21
Vigrel <eller.vini@gmail.com>
2021-11-10 17:14:55 -0300

    build(file): add Router to change pages on click Submit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,17 @@
 import Mood from "./components/Mood/forms";
+import Features from "./components/Mood/features";
 import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Mood />
+      <BrowserRouter className="App">
+        <Routes>
+          <Route path='' element={<Mood />} />
+          <Route path='/a' element={<Features />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
