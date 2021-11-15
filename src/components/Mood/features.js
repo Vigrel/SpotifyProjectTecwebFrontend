adb135b5e87ae17ed8a29e4b6d04e26a52eb5f03
Vigrel <eller.vini@gmail.com>
2021-11-15 11:44:03 -0300

    body: change structure body

---
+++ src/components/Mood/features.js
@@ -0,0 +1,62 @@
+import { Line } from 'rc-progress';
+import React, { useEffect, useState } from "react";
+import "./features.css";
+import { useParams, useLocation } from 'react-router-dom'
+import axios from 'axios'
+
+
+export default function Features() {
+    const id = useParams()
+    const [features, setFeatures] = useState([])
+
+    async function getForm() {
+        let response = await axios({
+            method: 'GET',
+            url: `https://moodspot.herokuapp.com/api/moods/${id.id}/`,
+        });
+        setFeatures(response.data)
+    }
+
+    useEffect(() => {
+        getForm()
+        // return () => {
+        //     setFeatures([]);
+        // };
+    }, []);
+
+    return (
+        <div className='background'>
+            <h1 className='app-name-features'>{features.album_name}, {features.track_name}, {features.artist_name}</h1>
+            <div className="bars-group">
+                <div className='group'>
+                    <p className='label'>acousticness</p>
+                    <Line className='bar' percent={features.acousticness * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>danceability</p>
+                    <Line className='bar' percent={features.danceability * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>energy</p>
+                    <Line className='bar' percent={features.energy * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>instrumentalness</p>
+                    <Line className='bar' percent={features.instrumentalness * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>liveness</p>
+                    <Line className='bar' percent={features.liveness * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>speechiness</p>
+                    <Line className='bar' percent={features.speechiness * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+                <div className='group'>
+                    <p className='label'>valence</p>
+                    <Line className='bar' percent={features.valence * 100} strokeWidth="2.5" strokeColor="#cdf564" trailColor="rgba(205, 245, 100, 0.1)" />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Mood/forms.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import "./forms.css";
+
+export default function Mood() {
+  const [mood, setMood] = useState('Tell us ur mood');
+  const [error, setError] = useState('');
+  const [features, setFeatures] = useState(null);
+  const [input, setInput] = useState('');
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (features != null) {
+      navigate(`/api/moods/${features.id}`)
+    }
+  }, [features]);
+
+  async function postForm() {
+    if (input == '') {
+      setError('insert valid link...')
+    }
+    if (mood == 'Tell us ur mood') {
+      setMood('insert mood...')
+    }
+
+    try {
+      let response = await axios({
+        method: 'post',
+        url: 'https://moodspot.herokuapp.com/api/moods/',
+        data: {
+          mood: mood,
+          track_url: input
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        }
+      });
+      setFeatures(response.data)
+    } catch {
+      setInput('')
+      setError('insert valid link...')
+    }
+  }
+
+
+  return (
+    <div className='background'>
+      <h1 className='app-name'>SoundMood</h1>
+      <div className='inputs' style={{ display: 'flex' }}>
+        <div className="dropdown">
+          <button className="dropbtn">{mood}</button>
+          <div className="dropdown-content">
+            <a onClick={() => setMood('😀 happy')}> happy</a>
+            <a onClick={() => setMood('😡 angry')}>angry</a>
+            <a onClick={() => setMood('😢 sad')}>sad</a>
+            <a onClick={() => setMood('😰 anxious')}>anxious</a>
+            <a onClick={() => setMood('😎 confident')}>confident</a>
+            <a onClick={() => setMood('😝 excited')}>excited</a>
+          </div>
+        </div>
+
+        <input
+          id='inputText'
+          type="url"
+          className="track_url_input"
+          placeholder={
+            error == '' ?
+            'u are listening...'
+            :
+            'insert valid link...'
+          }
+          value={input}
+          onChange={input => setInput(input.target.value)} />
+          
+        <button className="submit-btn" onClick={postForm}>
+          Submit
+        </button>
+      </div>
+
+    </div>
+  );
+}
